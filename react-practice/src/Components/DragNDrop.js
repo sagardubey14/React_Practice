689037452f1acae65d020d1b482c8f33f4e124dc
sagardubey14 <sagardubey353@gmail.com>
2024-12-02 15:01:30 +0530

    DnD-mid

--- react-practice/src/Components/DragNDrop.js
+++ react-practice/src/Components/DragNDrop.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
+
+const handleDrop = (e) => {
+  e.preventDefault();
+  const data = e.dataTransfer.getData("ele");
+  console.log(data);
+
+}
+
+const handleDragOver = (e)=>{
+  e.preventDefault()
+  
+  
+}
+const dragStart = (e)=>{
+  e.dataTransfer.setData("ele" , "fl");
+}
+
 function DragNDrop() {
   return (
-    <div>
-        <h1>Box</h1>
-      <div style={{border:"1px dashed black", height:"50vh", textAlign:"center", margin:"1px 10px"}}>
+    <div id='dnd'>
+      <h1>Box</h1>
+      <div draggable={true} onDragStart={dragStart} style={{ border: "1px dashed black", height: "50vh", textAlign: "center", margin: "1px 10px" }}>
         Drop file here
       </div>
-      <button style={{display:"block", margin:" auto"}}>Upload</button>
+      <button style={{ display: "block", margin: " auto" }}>Upload</button>
+      <div onDragOver={handleDragOver} onDrop={handleDrop}>
+        Drop here
+      </div>
     </div>
   )
 }
